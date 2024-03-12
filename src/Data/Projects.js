7e82f15b0a47248f64b0b02c9fbea8960c48aed5
GoodluckMlungusye <goodluckmlungusye@gmail.com>
2024-03-12 23:27:38 +0300

    Created projects json file

---
+++ src/Data/Projects.js
@@ -0,0 +1,105 @@
+const projects = [
+    {
+        'id': 1,
+        'name': 'UDVote',
+        'description': "A voting mobile application developed by flutter that consumes API from the UDVote-backend.",
+        'technology': 'Flutter',
+        'rate': 4,
+        'link': 'UDVote-app',
+        'color': '',
+        'image': ''
+    },
+    {
+        'id': 2,
+        'name': 'UDVote backend ',
+        'description': "Backend environment for UDVote mobile app developed by Laravel framework.",
+        'technology': 'Laravel',
+        'rate': 5,
+        'link': 'UDVote-backend',
+        'color': '',
+        'image': ''
+    },
+    {
+        'id': 3,
+        'name': 'Portfolio',
+        'description': "Personal portfolio front end website made from ReactJS.",
+        'technology': 'React',
+        'rate': 5,
+        'link': 'portfolio',
+        'color': '',
+        'image': ''
+    },
+    {
+        'id': 4,
+        'name': 'Portfolio backend',
+        'description': "Backend application for personal portfolio developed from Spring boot framework.",
+        'technology': 'Spring boot',
+        'rate': 3,
+        'link': 'portfolio-backend',
+        'color': '',
+        'image': ''
+    },
+    {
+        'id': 5,
+        'name': 'AfyaPro',
+        'description': "Website for requesting medical services for all hospitals under AfyaPro organization.",
+        'technology': 'PHP',
+        'rate': 3,
+        'link': 'afyapro',
+        'color': '',
+        'image': ''
+    },
+    {
+        'id': 6,
+        'name': 'React Router',
+        'description': "Dummy project to demonstrate react router DOM in depth.",
+        'technology': 'React',
+        'rate': 5,
+        'link': 'react-router',
+        'color': '',
+        'image': ''
+    },
+    {
+        'id': 7,
+        'name': 'Quiz App',
+        'description': "A simple flutter application designed with multiple organized quiz relating to software engineering field.",
+        'technology': 'Flutter',
+        'rate': 3,
+        'link': 'quiz-app',
+        'color': '',
+        'image': ''
+    },
+    {
+        'id': 8,
+        'name': 'E-commerce App',
+        'description': "Flutter application that enables a user to search for products, order products, and purchase them.",
+        'technology': 'Flutter',
+        'rate': 3,
+        'link': 'e-commerce-app',
+        'color': '',
+        'image': ''
+    },
+    {
+        'id': 9,
+        'name': 'Go-Do App',
+        'description': "Simple flutter todo application that enables one to manage scheduled tasks.",
+        'technology': 'Flutter',
+        'rate': 4,
+        'link': 'Go-Do-app',
+        'color': '',
+        'image': ''
+    },
+    {
+        'id': 10,
+        'name': 'Java Foundations',
+        'description': "Collective java concepts walkthrough.",
+        'technology': 'Java',
+        'rate': 4,
+        'link': 'java-foundation',
+        'color': '',
+        'image': ''
+    }
+]
+
+
+export default projects
